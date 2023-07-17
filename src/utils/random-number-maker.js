c58f29e86cc275ab8cdae3e4442378540fac2913
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-07-17 22:35:55 +0900

    📚docs :  js docs 문서 수정

--- src/utils/random-number-maker.js
+++ src/utils/random-number-maker.js
@@ -5,7 +5,8 @@
  * @return {Number} startNumber와 endNumber 사이에 랜덤한 값을 반환해줍니다.
  */
 const randomMaker = (startNumber, endNumber) => {
-	const makeNumber =  Math.floor(Math.random() * (endNumber - startNumber)) + startNumber
+	const makeNumber =
+		Math.floor(Math.random() * (endNumber - startNumber)) + startNumber
 	return makeNumber
 }
 
