1ae2bdfb557a72d9fe8a8427f5f799c9e69284fa
윤국현 <djaudwls7873@gmail.com>
2023-07-25 13:48:19 +0900

    🌈feat : 모바일 헤더, 푸터 퍼블리싱 완료

--- src/components/ui/organisms/MainHeader/MainHeader.jsx
+++ src/components/ui/organisms/MainHeader/MainHeader.jsx
@@ -1,117 +1,117 @@
 import { Box } from '@mui/material'
-import { selectApiTypeAtom } from 'atom/header/atom'
-import { API_KEYWORD } from 'consts/header/apiKeyword'
+import { useDevice } from 'hooks/mediaQuery/useDevice'
+import useMove from 'hooks/useMovePage'
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useSetRecoilState } from 'recoil'
 import styled from 'styled-components'
 import { flexCenter } from 'styles/common'
 
+import HeaderMobile from '../HeaderMobile/HeaderMobile'
 import HeaderCategory from './HeaderCategory'
 import HeaderChatAlarm from './HeaderChatAlarm'
 import HeaderScroll from './HeaderScroll'
 import HeaderSearch from './HeaderSearch'
 
 const MainHeader = props => {
-	const setSelectType = useSetRecoilState(selectApiTypeAtom)
-	const navigate = useNavigate()
-
-	const TypeHandling = API_KEY => {
-		setSelectType(API_KEY)
-		navigate(API_KEY)
-	}
+	const { linkMainPage, linkShareList, linkMyPage } = useMove()
+	const { isMobile, isMobileAndTablet } = useDevice()
 
 	return (
 		<>
-			<HeaderScroll />
+			{isMobile ? (
+				<HeaderMobile />
+			) : (
+				<>
+					<HeaderScroll />
 
-			<Box
-				sx={{
-					position: 'relative',
-					maxWidth: '1100px',
-					width: '100%',
-					height: '200px',
-					backgroundColor: '#FFFFFF',
-					padding: '16px',
-					boxSizing: 'border-box',
-					display: 'flex',
-					flexDirection: 'column',
-					justifyContent: 'center',
-					alignItems: 'center',
-					margin: 'auto',
-					zIndex: 30,
-				}}
-			>
-				<HeaderSearch />
+					<Box
+						sx={{
+							position: 'relative',
+							maxWidth: { xs: '100%', md: '1100px' },
+							width: '100%',
+							height: '200px',
+							backgroundColor: '#FFFFFF',
+							padding: '16px',
+							boxSizing: 'border-box',
+							display: 'flex',
+							flexDirection: 'column',
+							justifyContent: 'center',
+							alignItems: 'center',
+							margin: 'auto',
+							zIndex: 30,
+						}}
+					>
+						<HeaderSearch />
 
-				<S.Container>
-					<Box
-						sx={{
-							width: '100%',
-							display: 'flex',
-							justifyContent: 'flex-start',
-							height: 40,
-							padding: '10px 0 10px 0',
-							fontSize: '20px',
-						}}
-					>
-						<HeaderCategory />
-					</Box>
-					<Box
-						onClick={() => TypeHandling(API_KEYWORD.SECONDHAND_DEALS)}
-						sx={{
-							width: '100%',
-							display: 'flex',
-							justifyContent: 'flex-start',
-							height: 40,
-							padding: '10px 0 10px 0',
-							fontSize: '20px',
-							'&:hover': {
-								color: '#009d91',
-							},
-						}}
-					>
-						<span>중고거래</span>
-					</Box>
-					<Box
-						onClick={() => TypeHandling(API_KEYWORD.FREE_SHARING)}
-						sx={{
-							width: '100%',
-							display: 'flex',
-							justifyContent: 'flex-start',
-							height: 40,
-							padding: '10px 0 10px 0',
-							fontSize: '20px',
-							'&:hover': {
-								color: '#009d91',
-							},
-						}}
-					>
-						<span>무료나눔</span>
-					</Box>
-					<Box
-						onClick={() => TypeHandling(API_KEYWORD.POPULAR_PRODUCTS)}
-						sx={{
-							width: '100%',
-							display: 'flex',
-							justifyContent: 'flex-start',
-							height: 40,
-							padding: '10px 0 10px 0',
-							fontSize: '20px',
-							'&:hover': {
-								color: '#009d91',
-							},
-						}}
-					>
-						<span>인기상품</span>
-					</Box>
+						<S.Container>
+							<Box
+								sx={{
+									width: '100%',
+									display: 'flex',
+									justifyContent: 'flex-start',
+									height: 40,
+									padding: '10px 0 10px 0',
+									fontSize: '20px',
+								}}
+							>
+								<HeaderCategory />
+							</Box>
+							<Box
+								onClick={linkMainPage}
+								sx={{
+									width: '100%',
+									display: 'flex',
+									justifyContent: 'flex-start',
+									height: 40,
+									padding: '10px 0 10px 0',
+									fontSize: '20px',
+									'&:hover': {
+										color: '#009d91',
+									},
+								}}
+							>
+								<span>메인페이지</span>
+							</Box>
+							<Box
+								onClick={linkShareList}
+								sx={{
+									width: '100%',
+									display: 'flex',
+									justifyContent: 'flex-start',
+									height: 40,
+									padding: '10px 0 10px 0',
+									fontSize: '20px',
+									'&:hover': {
+										color: '#009d91',
+									},
+								}}
+							>
+								<span>무료나눔</span>
+							</Box>
+							<Box
+								onClick={linkMyPage}
+								sx={{
+									width: '100%',
+									display: 'flex',
+									justifyContent: 'flex-start',
+									height: 40,
+									padding: '10px 0 10px 0',
+									fontSize: '20px',
+									'&:hover': {
+										color: '#009d91',
+									},
+								}}
+							>
+								<span>마이페이지</span>
+							</Box>
 
-					<S.ParadiseSection>
-						<span>파라다이스</span> 서비스 소개
-					</S.ParadiseSection>
-					<HeaderChatAlarm />
-				</S.Container>
-			</Box>
+							<S.ParadiseSection>
+								<span>파라다이스</span> 서비스 소개
+							</S.ParadiseSection>
+							<HeaderChatAlarm />
+						</S.Container>
+					</Box>
+				</>
+			)}
 		</>
 	)
 }
@@ -134,10 +134,14 @@ const ParadiseSection = styled.div`
 	justify-content: center;
 	align-items: center;
 	padding: 10px 0;
-	font-size: 16px;
+	font-size: ${({ isMobileAndTablet }) =>
+		isMobileAndTablet ? '14px' : '16px'};
 	border: 1px solid #999;
 	border-radius: 20px;
 	color: #999;
+	margin-left: ${({ isMobileAndTablet }) => (isMobileAndTablet ? '10px' : '0')};
+	margin-right: ${({ isMobileAndTablet }) =>
+		isMobileAndTablet ? '10px' : '0'};
 
 	cursor: pointer;
 	span {
