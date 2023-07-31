4e2e4ca9e8b070300f2910ed95968a35cdc4e8a2
uniend <dudnsgldns@naver.com>
2023-08-01 00:34:20 +0900

    📝fix : 요청사항 수정

--- src/components/ui/organisms/MyPageSection/MyPageInfo.jsx
+++ src/components/ui/organisms/MyPageSection/MyPageInfo.jsx
@@ -46,128 +46,126 @@ const MyPageInfo = () => {
 
 	return (
 		<S.Wrap isdesk={isDesk.toString()}>
-				<S.Wrapper>
-					<S.Title>회원 정보 수정</S.Title>
-					<S.Notice>
-						<span>
-							<span className="secondary">*필수항목</span>은 꼭 입력해주세요
-						</span>
-					</S.Notice>
-					<S.Form onSubmit={handleSubmit(onSubmit)}>
-						<S.CheckContent>
-							<S.FromLabel>
-								이메일
-							</S.FromLabel>
-							<InputGroup className='readonly'>
-								<Input
-									readOnly={true}
-									name="email"
-									placeholder={'이메일을 입력해주세요'}
-									{...register('email')}
-									error={errors.email?.message}
-								/>
-							</InputGroup>
-						</S.CheckContent>
-						<S.CheckContent>
-							<S.FromLabel>
-								닉네임<span className="secondary">*</span>
-							</S.FromLabel>
-							<InputGroup>
-								<Input
-									name="nickname"
-									placeholder={'닉네임을 입력헤주세요'}
-									width={'322'}
-									{...register('nickname')}
-									error={errors.nickname?.message}
-								/>
-								<S.CommonButton
-									type="button"
-									label={'중복확인'}
-									variant={'primary-outlined'}
-								/>
-							</InputGroup>
-						</S.CheckContent>
-						<S.Content>
-							<S.FromLabel>
-								새 비밀번호<span className="secondary"> * </span>
-							</S.FromLabel>
-							<InputGroup>
-								<Input
-									name="password"
-									placeholder={'비밀번호를 입력해주세요'}
-									{...register('password')}
-									error={errors.password?.message}
-								/>
-							</InputGroup>
-						</S.Content>
-						<S.Content>
-							<S.FromLabel>
-								새 비밀번호 확인<span className="secondary">* </span>
-							</S.FromLabel>
-							<InputGroup>
-								<Input
-									name="passwordconfirm"
-									placeholder={'비밀번호 확인을 입력해주세요'}
-									{...register('passwordconfirm')}
-									error={errors.passwordconfirm?.message}
-								/>
-							</InputGroup>
-						</S.Content>
-						<S.CheckContent>
-							<S.FromLabel>
-								휴대폰 번호<span className="secondary">* </span>
-							</S.FromLabel>
-							<InputGroup>
-								<Input
-									name="phone"
-									placeholder={'휴대폰 번호를 입력해주세요 (ex. 010-0000-0000)'}
-									{...register('phone')}
-									error={errors.phone?.message}
-								/>
-							</InputGroup>
-						</S.CheckContent>
-						<S.CheckContent>
-							<S.FromLabel>
-								지역선택<span className="secondary">*</span>
-							</S.FromLabel>
-							<InputGroup>
-								<Input
-									name="address"
-									placeholder={'검색 버튼을 눌러주세요'}
-									width={'322'}
-									{...register('address')}
-									error={errors.address?.message}
-								/>
-								<S.CommonButton
-									type="button"
-									label={'검색'}
-									variant={'primary-outlined'}
-									onClick={handleOpen}
-								/>
-								{isPopUp && (
-									<PopUp
-										handleClose={handleClose}
-										handleAddress={handleAddress}
-									/>
-								)}
-							</InputGroup>
-						</S.CheckContent>
-						<S.SignUpButton>
-							<Button
+			<S.Wrapper>
+				<S.Title>회원 정보 수정</S.Title>
+				<S.Notice>
+					<span>
+						<span className="secondary">*필수항목</span>은 꼭 입력해주세요
+					</span>
+				</S.Notice>
+				<S.Form onSubmit={handleSubmit(onSubmit)}>
+					<S.CheckContent>
+						<S.FromLabel>이메일</S.FromLabel>
+						<InputGroup className="readonly">
+							<Input
+								readOnly={true}
+								name="email"
+								placeholder={'이메일을 입력해주세요'}
+								{...register('email')}
+								error={errors.email?.message}
+							/>
+						</InputGroup>
+					</S.CheckContent>
+					<S.CheckContent>
+						<S.FromLabel>
+							닉네임<span className="secondary">*</span>
+						</S.FromLabel>
+						<InputGroup>
+							<Input
+								name="nickname"
+								placeholder={'닉네임을 입력헤주세요'}
+								width={'322'}
+								{...register('nickname')}
+								error={errors.nickname?.message}
+							/>
+							<S.CommonButton
 								type="button"
-								label={'탈퇴하기'}
-								size={'large'}
+								label={'중복확인'}
 								variant={'primary-outlined'}
 							/>
-							<Button
-								type="submit"
-								label={'회원정보 수정'}
-								size={'large'}
-								variant={'primary'}
+						</InputGroup>
+					</S.CheckContent>
+					<S.Content>
+						<S.FromLabel>
+							새 비밀번호<span className="secondary"> * </span>
+						</S.FromLabel>
+						<InputGroup>
+							<Input
+								name="password"
+								placeholder={'비밀번호를 입력해주세요'}
+								{...register('password')}
+								error={errors.password?.message}
 							/>
-						</S.SignUpButton>
-					</S.Form>
-				</S.Wrapper>
+						</InputGroup>
+					</S.Content>
+					<S.Content>
+						<S.FromLabel>
+							새 비밀번호 확인<span className="secondary">* </span>
+						</S.FromLabel>
+						<InputGroup>
+							<Input
+								name="passwordconfirm"
+								placeholder={'비밀번호 확인을 입력해주세요'}
+								{...register('passwordconfirm')}
+								error={errors.passwordconfirm?.message}
+							/>
+						</InputGroup>
+					</S.Content>
+					<S.CheckContent>
+						<S.FromLabel>
+							휴대폰 번호<span className="secondary">* </span>
+						</S.FromLabel>
+						<InputGroup>
+							<Input
+								name="phone"
+								placeholder={'휴대폰 번호를 입력해주세요 (ex. 010-0000-0000)'}
+								{...register('phone')}
+								error={errors.phone?.message}
+							/>
+						</InputGroup>
+					</S.CheckContent>
+					<S.CheckContent>
+						<S.FromLabel>
+							지역선택<span className="secondary">*</span>
+						</S.FromLabel>
+						<InputGroup>
+							<Input
+								name="address"
+								placeholder={'검색 버튼을 눌러주세요'}
+								width={'322'}
+								{...register('address')}
+								error={errors.address?.message}
+							/>
+							<S.CommonButton
+								type="button"
+								label={'검색'}
+								variant={'primary-outlined'}
+								onClick={handleOpen}
+							/>
+							{isPopUp && (
+								<PopUp
+									handleClose={handleClose}
+									handleAddress={handleAddress}
+								/>
+							)}
+						</InputGroup>
+					</S.CheckContent>
+					<S.SignUpButton>
+						<Button
+							type="button"
+							label={'탈퇴하기'}
+							size={'large'}
+							variant={'primary-outlined'}
+						/>
+						<Button
+							type="submit"
+							label={'회원정보 수정'}
+							size={'large'}
+							variant={'primary'}
+						/>
+					</S.SignUpButton>
+				</S.Form>
+			</S.Wrapper>
 		</S.Wrap>
 	)
 }
@@ -210,8 +208,8 @@ S.Form = styled.form`
 S.CheckContent = styled.div`
 	padding: 15px 0;
 
-	.readonly{
-		:focus{
+	.readonly {
+		:focus {
 			outline: none;
 		}
 	}
