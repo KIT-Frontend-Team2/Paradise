1d39f6c5fb01ec68fcf98e6228b3662952c2f633
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-08-04 23:18:06 +0900

    💄style : 코드 포맷팅누락

--- src/apis/service/user.api.js
+++ src/apis/service/user.api.js
@@ -1,108 +1,108 @@
 import axios from 'axios'
 import API_KEY from 'consts/ApiKey'
 
-import {BASE_URL} from '../../consts/api'
-import {axiosInstance} from '../axiosInstance'
+import { BASE_URL } from '../../consts/api'
+import { axiosInstance } from '../axiosInstance'
 
 const userService = {
-    signUp: userInfo => {
-        return axios.post(BASE_URL + API_KEY.API + API_KEY.USER, {
-            ...userInfo,
-        })
-    },
+	signUp: userInfo => {
+		return axios.post(BASE_URL + API_KEY.API + API_KEY.USER, {
+			...userInfo,
+		})
+	},
 
-    login: (email, pw) => {
-        return axios.post(BASE_URL + API_KEY.API + API_KEY.USER + '/login', {
-            email,
-            pw,
-        })
-    },
+	login: (email, pw) => {
+		return axios.post(BASE_URL + API_KEY.API + API_KEY.USER + '/login', {
+			email,
+			pw,
+		})
+	},
 
-    logOut: () => {
-        return axiosInstance.get(API_KEY.API + API_KEY.USER + '/logout')
-    },
+	logOut: () => {
+		return axiosInstance.get(API_KEY.API + API_KEY.USER + '/logout')
+	},
 
-    checkNickName: nickname => {
-        return axiosInstance.get(API_KEY.API + API_KEY.USER + '/check/nickname', {
-            params: {nickname},
-        })
-    },
+	checkNickName: nickname => {
+		return axiosInstance.get(API_KEY.API + API_KEY.USER + '/check/nickname', {
+			params: { nickname },
+		})
+	},
 
-    checkEmail: email => {
-        return axiosInstance.get(API_KEY.API + API_KEY.USER + '/check/email', {
-            params: {email},
-        })
-    },
+	checkEmail: email => {
+		return axiosInstance.get(API_KEY.API + API_KEY.USER + '/check/email', {
+			params: { email },
+		})
+	},
 
-    getRefreshToken: () => {
-        return axiosInstance.get(
-            BASE_URL + API_KEY.API + API_KEY.USER + '/refreshToken',
-            {
-                params: {
-                    email: localStorage.getItem('email'),
-                    pw: localStorage.getItem('pw'),
-                },
-            },
-        )
-    },
+	getRefreshToken: () => {
+		return axiosInstance.get(
+			BASE_URL + API_KEY.API + API_KEY.USER + '/refreshToken',
+			{
+				params: {
+					email: localStorage.getItem('email'),
+					pw: localStorage.getItem('pw'),
+				},
+			},
+		)
+	},
 
-    getUserInfo: () => {
-        return axiosInstance.get(API_KEY.API + API_KEY.USER + '/info')
-    },
+	getUserInfo: () => {
+		return axiosInstance.get(API_KEY.API + API_KEY.USER + '/info')
+	},
 
-    getMypage: () => {
-        return axiosInstance.get(API_KEY.API + API_KEY.USER + API_KEY.MYPAGE)
-    },
+	getMypage: () => {
+		return axiosInstance.get(API_KEY.API + API_KEY.USER + API_KEY.MYPAGE)
+	},
 
-    changeUserInfo: UserInfo => {
-        return axiosInstance.patch(API_KEY.API + API_KEY.USER, {...UserInfo})
-    },
+	changeUserInfo: UserInfo => {
+		return axiosInstance.patch(API_KEY.API + API_KEY.USER, { ...UserInfo })
+	},
 
-    changeUserProfile: image => {
-        return axiosInstance.patch(API_KEY.API + API_KEY.USER + '/profile', {
-            image,
-        })
-    },
+	changeUserProfile: image => {
+		return axiosInstance.patch(API_KEY.API + API_KEY.USER + '/profile', {
+			image,
+		})
+	},
 
-    changeUserPassword: pw => {
-        return axiosInstance.patch(API_KEY.API + API_KEY.USER + '/password', {pw})
-    },
+	changeUserPassword: pw => {
+		return axiosInstance.patch(API_KEY.API + API_KEY.USER + '/password', { pw })
+	},
 
-    getMyPageProductInfo: filter => {
-        return axiosInstance.get(
-            API_KEY.API + API_KEY.USER + '/my-page/product-list',
-            {
-                params: {
-                    ...filter,
-                },
-            },
-        )
-    },
+	getMyPageProductInfo: filter => {
+		return axiosInstance.get(
+			API_KEY.API + API_KEY.USER + '/my-page/product-list',
+			{
+				params: {
+					...filter,
+				},
+			},
+		)
+	},
 
-    getMyPageLikeProductInfo: filter => {
-        return axiosInstance.get(
-            API_KEY.API + API_KEY.USER + '/my-page/product-list',
-            {
-                params: {
-                    ...filter,
-                },
-            },
-        )
-    },
+	getMyPageLikeProductInfo: filter => {
+		return axiosInstance.get(
+			API_KEY.API + API_KEY.USER + '/my-page/product-list',
+			{
+				params: {
+					...filter,
+				},
+			},
+		)
+	},
 
-    getMyPageAccountBook: (page, category, start, end) => {
-        return axiosInstance.get(
-            API_KEY.API + API_KEY.USER + '/my-page/like-product-list',
-            {
-                params: {
-                    page,
-                    category,
-                    start,
-                    end,
-                },
-            },
-        )
-    },
+	getMyPageAccountBook: (page, category, start, end) => {
+		return axiosInstance.get(
+			API_KEY.API + API_KEY.USER + '/my-page/like-product-list',
+			{
+				params: {
+					page,
+					category,
+					start,
+					end,
+				},
+			},
+		)
+	},
 }
 
 export default userService
