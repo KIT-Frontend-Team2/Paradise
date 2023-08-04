ed2ff102a8ad264ecc134122aa40915edf511f25
uniend <dudnsgldns@naver.com>
2023-08-04 23:16:21 +0900

    📝fix : 충돌 해결시 코드 누락 해결

--- src/components/templates/AuthPageTemplate/SignUpTemplate.jsx
+++ src/components/templates/AuthPageTemplate/SignUpTemplate.jsx
@@ -1,5 +1,6 @@
 import { yupResolver } from '@hookform/resolvers/yup'
 import Container from 'components/layout/Container'
+import PopUp from 'components/modal/MapModal/AddressModal'
 import Button from 'components/ui/atoms/Button/Button'
 import Input from 'components/ui/atoms/Input/Input'
 import InputGroup from 'components/ui/molecules/InputGroup/InputGroup'
@@ -7,65 +8,152 @@ import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { styled } from 'styled-components'
 
-import Checkbox from '../../../assets/images/checkbox.png'
+import { ERROR_MESSAGE } from '../../../consts/api'
 import useUserAPi from '../../../hooks/service/user.service'
-import useMove from '../../../hooks/useMovePage'
-import { Validation2 } from './validation'
+import { Validation } from './validation'
 
-const Login = ({ setState }) => {
-	const { linkMainPage } = useMove()
-	const [request, setRequest] = useState(false)
+const SignUp = ({ setState }) => {
 	const {
 		register,
 		handleSubmit,
-		watch,
 		formState: { errors },
+		setValue,
+		watch,
 	} = useForm({
-		mode: 'onchange',
-		resolver: yupResolver(Validation2),
+		mode: 'onChange',
+		resolver: yupResolver(Validation),
 	})
+	const [isPopUp, setIsPopUp] = useState(false)
+	const [checkState, setCheckState] = useState([false, false])
+	const [request, setRequest] = useState(false)
 
-	const { mutateAsync } = useUserAPi.login()
+	const { mutateAsync: checkEmail } = useUserAPi.checkEmail(watch('email'))
+	const { mutateAsync: checkNickName } = useUserAPi.checkNickName(
+		watch('nickname'),
+	)
+	const { mutateAsync: signUp } = useUserAPi.signup()
 
-	const onSubmit = async () => {
+	const onSubmit = async e => {
 		try {
 			if (request) return
 			setRequest(true)
-			await mutateAsync({
-				email: watch('email'),
-				pw: watch('password'),
-			})
-			linkMainPage()
+			if (checkState.filter(prev => prev === true).length === 2) {
+				const userInfo = {
+					region: e.address,
+					email: e.email,
+					nickName: e.nickname,
+					pw: e.password,
+					phone: e.phone,
+				}
+				await signUp(userInfo)
+				alert('축하합니다 회원가입이 완료되었습니다.')
+				setState(true)
+			} else {
+				if (checkState[0] === false)
+					return alert('이메일 중복검사를 진행해주세요')
+				if (checkState[1] === false)
+					return alert('닉네임 중복검사를 진행해주세요')
+			}
 		} catch (err) {
-			alert(
-				'이메일 또는 비밀번호를 잘못 입력했습니다.\n' +
-					'입력하신 내용을 다시 확인해주세요.',
-			)
 			setRequest(false)
+			alert(ERROR_MESSAGE)
+		}
+	}
+
+	const handleOpen = () => {
+		setIsPopUp(true)
+	}
+
+	const handleClose = () => {
+		setIsPopUp(false)
+	}
+
+	const handleAddress = fullAddress => {
+		setValue('address', fullAddress)
+	}
+
+	const handleEmail = async () => {
+		if (request) return
+		setRequest(true)
+		try {
+			if (watch('email').trim().length === 0) return alert('값을 입력해주세요')
+			if (errors.email) return alert('이메일 양식을 지켜주세요')
+			await checkEmail(watch('email'))
+			if (confirm('해당 이메일로 확정하시겠습니까?')) {
+				setCheckState(prev => {
+					let newState = [...prev]
+					newState[0] = true
+					return newState
+				})
+			}
+			setRequest(false)
+		} catch (err) {
+			setRequest(false)
+			alert(err.message)
+		}
+	}
+
+	const handleNickName = async () => {
+		if (request) return
+		setRequest(true)
+		try {
+			if (watch('nickname').trim().length === 0)
+				return alert('값을 입력해주세요')
+			if (errors.nickname) return alert('닉네임 양식을 지켜주세요')
+			await checkNickName(watch('nickname'))
+			if (confirm('해당 닉네임으로 확정하시겠습니까?')) {
+				setCheckState(prev => {
+					let newState = [...prev]
+					newState[1] = true
+					return newState
+				})
+			}
+			setRequest(false)
+		} catch (err) {
+			setRequest(false)
+			alert(err.message)
 		}
 	}
 
 	return (
 		<Container>
 			<S.Wrapper>
-				<S.Title>로그인</S.Title>
+				<S.Title>회원가입</S.Title>
+				<S.Notice>
+					<span>
+						<span className="secondary">*필수항목</span>은 꼭 입력해주세요
+					</span>
+				</S.Notice>
 				<S.Form onSubmit={handleSubmit(onSubmit)}>
-					<S.Content>
-						<S.FromLabel>이메일</S.FromLabel>
+					<S.CheckContent>
+						<S.FromLabel>
+							이메일<span className="secondary">*</span>
+						</S.FromLabel>
 						<InputGroup>
 							<Input
-								placeholder={'이메일을 입력해주세요'}
-								{...register('email')}
+								readOnly={checkState[0]}
 								name="email"
+								placeholder={'이메일을 입력해주세요'}
+								width={'322'}
+								{...register('email')}
 								error={errors.email?.message}
 							/>
+							<S.CommonButton
+								disabled={checkState[0]}
+								onClick={handleEmail}
+								type="button"
+								label={'중복확인'}
+								variant={'primary-outlined'}
+							/>
 						</InputGroup>
-					</S.Content>
+					</S.CheckContent>
 					<S.Content>
-						<S.FromLabel>비밀번호</S.FromLabel>
+						<S.FromLabel>
+							비밀번호<span className="secondary"> * </span>
+						</S.FromLabel>
 						<InputGroup>
 							<Input
-								type={'password'}
+								type="password"
 								name="password"
 								placeholder={'비밀번호를 입력해주세요'}
 								{...register('password')}
@@ -73,108 +161,165 @@ const Login = ({ setState }) => {
 							/>
 						</InputGroup>
 					</S.Content>
-					<S.Checkradio>
-						{/*<input type="checkbox" />*/}
-						{/*<S.FromLabel>*/}
-						{/*	<label className="checklabel">아이디 기억하기</label>*/}
-						{/*</S.FromLabel>*/}
-					</S.Checkradio>
-					<S.Button type="submit" label={'로그인'} size={'full'} />
-					<S.SignUp
-						type="button"
-						label={'회원가입'}
-						size={'full'}
-						onClick={() => setState(false)}
-					>
-						회원가입
-					</S.SignUp>
+					<S.Content>
+						<S.FromLabel>
+							비밀번호 확인<span className="secondary">* </span>
+						</S.FromLabel>
+						<InputGroup>
+							<Input
+								type="password"
+								name="passwordConfirm"
+								placeholder={'비밀번호 확인을 입력해주세요'}
+								{...register('passwordConfirm')}
+								error={errors.passwordConfirm?.message}
+							/>
+						</InputGroup>
+					</S.Content>
+					<S.CheckContent>
+						<S.FromLabel>
+							닉네임<span className="secondary">*</span>
+						</S.FromLabel>
+						<InputGroup>
+							<Input
+								readOnly={checkState[1]}
+								name="nickname"
+								placeholder={'닉네임을 입력헤주세요'}
+								width={'322'}
+								{...register('nickname')}
+								error={errors.nickname?.message}
+							/>
+							<S.CommonButton
+								disabled={checkState[1]}
+								onClick={handleNickName}
+								type="button"
+								label={'중복확인'}
+								variant={'primary-outlined'}
+							/>
+						</InputGroup>
+					</S.CheckContent>
+					<S.CheckContent>
+						<S.FromLabel>
+							휴대폰 번호<span className="secondary">* </span>
+						</S.FromLabel>
+						<InputGroup>
+							<Input
+								name="phone"
+								placeholder={'휴대폰 번호를 입력해주세요 (ex. 010-0000-0000)'}
+								{...register('phone')}
+								error={errors.phone?.message}
+							/>
+						</InputGroup>
+					</S.CheckContent>
+					<S.CheckContent>
+						<S.FromLabel>
+							지역선택<span className="secondary">*</span>
+						</S.FromLabel>
+						<InputGroup>
+							<Input
+								readOnly
+								name="address"
+								placeholder={'검색 버튼을 눌러주세요'}
+								width={'322'}
+								{...register('address')}
+								error={errors.address?.message}
+							/>
+							<S.CommonButton
+								type="button"
+								label={'검색'}
+								variant={'primary-outlined'}
+								onClick={handleOpen}
+							/>
+							{isPopUp && (
+								<PopUp
+									handleClose={handleClose}
+									handleAddress={handleAddress}
+								/>
+							)}
+						</InputGroup>
+					</S.CheckContent>
+					<S.SignUpButton>
+						<Button type="submit" label={'회원가입'} size={'full'} />
+					</S.SignUpButton>
+					<S.LoginButton onClick={() => setState(true)}>
+						<Button type="button" label={'로그인'} size={'full'} />
+					</S.LoginButton>
 				</S.Form>
 			</S.Wrapper>
 		</Container>
 	)
 }
 
-export default Login
+export default SignUp
 
-const S = {}
+export const S = {}
 
 S.Wrapper = styled.div`
 	width: 100%;
 	max-width: 480px;
 	margin: 90px auto 200px;
-	text-align: center;
+	position: relative;
 `
 S.Title = styled.h2`
 	font-size: 32px;
 	font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
 	text-align: center;
-	margin-bottom: 30px;
+	margin-bottom: 48px;
 `
+S.Notice = styled.div`
+	text-align: right;
+	font-size: ${({ theme }) => theme.FONT_SIZE.small};
+	margin-bottom: 20px;
 
+	.secondary {
+		color: ${({ theme }) => theme.PALETTE.secondary};
+		font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
+	}
+`
 S.Form = styled.form`
 	margin-bottom: 48px;
-	text-align: left;
+`
+
+S.CheckContent = styled.div`
+	padding: 15px 0;
 `
 S.Content = styled.div`
-	&:first-child {
-		padding: 20px 0;
-	}
-
-	&:last-child {
-		padding-bottom: 0;
-	}
-`
-
-S.Checkradio = styled.div`
 	padding: 15px 0;
-	display: flex;
-	align-items: center;
-
-	> [type='checkbox'] {
-		appearance: none;
-		width: 1.25em;
-		height: 1.25em;
-		border: 1px solid ${({ theme }) => theme.PALETTE.primary[100]};
-		cursor: pointer;
-		transition: 0.6s;
-		margin-right: 10px;
-	}
-
-	> [type='checkbox']:checked {
-		background-image: url(${Checkbox});
-		background-size: cover;
-		background-repeat: no-repeat;
-		background-position: center;
-	}
 `
+
 S.FromLabel = styled.div`
 	padding-bottom: 10px;
 	font-size: ${({ theme }) => theme.FONT_SIZE.xlarge};
 	font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
-	text-align: left;
 
-	.checklabel {
-		font-weight: ${({ theme }) => theme.FONT_WEIGHT.light};
-		font-size: ${({ theme }) => theme.FONT_SIZE.xsmall};
-		vertical-align: -5px;
+	.secondary {
+		color: ${({ theme }) => theme.PALETTE.secondary};
+		font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
 	}
 `
 
-S.Button = styled(Button)`
-	margin-bottom: 15px;
-	font-size: ${({ theme }) => theme.FONT_SIZE.xlarge};
-	font-weight: ${({ theme }) => theme.FONT_WEIGHT.medium};
+S.SignUpButton = styled.div`
+	margin-top: 30px;
+
+	button {
+		margin-bottom: 15px;
+		font-size: ${({ theme }) => theme.FONT_SIZE.xlarge};
+		font-weight: ${({ theme }) => theme.FONT_WEIGHT.medium};
+	}
 `
 
-S.SignUp = styled(Button)`
-	background-color: rgba(255, 255, 255, 100%);
-	border: 1px solid #cccccc;
-	color: ${({ theme }) => theme.PALETTE.black};
-	font-size: ${({ theme }) => theme.FONT_SIZE.xlarge};
-	font-weight: ${({ theme }) => theme.FONT_WEIGHT.medium};
+S.CommonButton = styled(Button)`
+	text-overflow: ellipsis;
+`
 
-	&:hover {
-		background-color: rgba(255, 255, 255, 65%);
+S.LoginButton = styled(S.SignUpButton)`
+	button {
+		background-color: rgba(255, 255, 255, 100%);
 		border: 1px solid #cccccc;
+		color: ${({ theme }) => theme.PALETTE.black};
+
+		&:hover {
+			border: 1px solid #cccccc;
+			background-color: rgba(255, 255, 255, 65%);
+		}
 	}
 `
